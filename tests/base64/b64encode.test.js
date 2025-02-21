2c6ddd9b7390408990101de72519ff95c715010f
LCluber <ludovic.cluber@gmail.com>
2025-02-22 00:01:03 +0100

    refactor base64 encoding tests for improved clarity and consistency; remove redundant cases

--- tests/base64/b64encode.test.js
+++ tests/base64/b64encode.test.js
@@ -1,143 +1,57 @@
 import { b64Encode } from "../../dist/ch";
 
-const estring = "c3RyaW5n";
-const e1 = "MQ==";
-const e2 = "ODk=";
-const e3 = "ODlydA==";
-const e4 = "MS41";
-const e5 = "OC45cnQ=";
+const estring = "c3RyaW5n"; // string
+const e1 = "MQ=="; // 1
+const e1s = "MQ";
+const e2 = "ODk="; // 89
+const e2s = "ODk"; // 89
+const e3 = "ODlydA=="; // 89rt
+const e3s = "ODlydA"; // 89rt
+const e4 = "MS41"; // 1.5
+const e5 = "OC45cnQ="; // 8.9rt
+const e5s = "OC45cnQ"; // 8.9rt
 
-
-
-test("sends NaN to b64Encode", () => {
-  expect(b64Encode(Number.NaN)).toBe("");
-});
-
-test("sends null to b64Encode", () => {
-  expect(b64Encode(null)).toBe("");
-});
-
-test("sends undefined to b64Encode", () => {
-  expect(b64Encode(undefined)).toBe(false);
-});
-
-const s1 = Symbol();
-test("sends symbol to b64Encode", () => {
-  expect(isNumber(s1, false) && b64Encode(s1)).toBe(false);
-});
-
-test("sends true to b64Encode", () => {
-  expect(b64Encode(true)).toBe(false);
-});
-
-test("sends false to b64Encode", () => {
-  expect(b64Encode(false)).toBe(false);
-});
 
 test("sends string to b64Encode", () => {
-  expect(b64Encode("string")).toBe("c3RyaW5n");
+  expect(b64Encode("string")).toBe(estring);
 });
 
 test("sends figure as string to b64Encode", () => {
-  expect(b64Encode("1")).toBe("MQ==");
+  expect(b64Encode("1")).toBe(e1s);
 });
 
 test("sends number as string to b64Encode", () => {
-  expect(b64Encode("89")).toBe("ODk=");
+  expect(b64Encode("89")).toBe(e2s);
 });
 
 test("sends number in string to b64Encode", () => {
-  expect(b64Encode("89rt")).toBe("ODlydA==");
+  expect(b64Encode("89rt")).toBe(e3s);
 });
 
 test("sends figure as string to b64Encode without typeCheck", () => {
-  expect(b64Encode("1", false)).toBe("MQ==");
+  expect(b64Encode("1", false)).toBe(e1);
 });
 
 test("sends number as string to b64Encode without typeCheck", () => {
-  expect(b64Encode("89", false)).toBe("ODk=");
+  expect(b64Encode("89", false)).toBe(e2);
 });
 
 test("sends number in string to b64Encode without typeCheck", () => {
-  expect(b64Encode("89rt", false)).toBe("ODlydA==");
+  expect(b64Encode("89rt", false)).toBe(e3);
 });
 
 test("sends float as string to b64Encode", () => {
-  expect(b64Encode("1.5")).toBe(false);
+  expect(b64Encode("1.5")).toBe(e4);
 });
 
 test("sends number in string to b64Encode", () => {
-  expect(b64Encode("8.9rt")).toBe(false);
+  expect(b64Encode("8.9rt")).toBe(e5s);
 });
 
 test("sends float as string to b64Encode without typeCheck", () => {
-  expect(b64Encode("1.5", false)).toBe(true);
+  expect(b64Encode("1.5", false)).toBe(e4);
 });
 
 test("sends float in string to b64Encode without typeCheck", () => {
-  expect(b64Encode("8.9rt", false)).toBe(false);
-});
-
-test("sends positive even integer to b64Encode", () => {
-  expect(b64Encode(2)).toBe(false);
-});
-
-test("sends positive odd integer to b64Encode", () => {
-  expect(b64Encode(1)).toBe(false);
-});
-
-test("sends zero to b64Encode", () => {
-  expect(b64Encode(0)).toBe(false);
-});
-
-test("sends positive float to b64Encode", () => {
-  expect(b64Encode(1.1)).toBe(true);
-});
-
-test("sends negative odd integer to b64Encode", () => {
-  expect(b64Encode(-1)).toBe(false);
-});
-
-test("sends negative even integer to b64Encode", () => {
-  expect(b64Encode(-2)).toBe(false);
-});
-
-test("sends negative float to b64Encode", () => {
-  expect(b64Encode(-1.1)).toBe(true);
-});
-
-test("sends object to b64Encode", () => {
-  expect(b64Encode({})).toBe(false);
-});
-
-test("sends empty array to b64Encode", () => {
-  expect(b64Encode([])).toBe(false);
-});
-
-const json = `{
-  "actor": {
-    "name": "Tom Cruise",
-    "age": 56,
-    "Born At": "Syracuse, NY",
-    "Birthdate": "July 3 1962",
-    "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
-  }
-}`;
-
-test("sends json to b64Encode", () => {
-  expect(b64Encode(json)).toBe(false);
-});
-
-const invalidjson = `{
-  "actor: {
-    "name": "Tom Cruise",
-    "age": 56
-    "Born At": "Syracuse, NY",
-    "Birthdate": "July 3 1962",
-    "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
-  }
-}`;
-
-test("sends invalid json to b64Encode", () => {
-  expect(b64Encode(invalidjson)).toBe(false);
+  expect(b64Encode("8.9rt", false)).toBe(e5);
 });
